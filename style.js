4c80b17a1cb80f2521f998481f50e0ffc3538a3b
Ngọc Hânn <131350494+ngochan04@users.noreply.github.com>
2023-09-21 14:38:36 +0700

    Add files via upload

---
+++ style.js
@@ -0,0 +1,13 @@
+var date = 3
+if (date === 2){
+    console.log('Hom nay la thu hai')
+}
+else if (date === 3){
+    console.log('Hom nay la thu ba ')
+}
+else if (date === 4){
+    console.log('Hom nay la thu tu ')
+}
+else{
+    alert('Vui long kiem tra lai thong tin da nhap')
+}
